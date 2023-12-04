f8cb418547713506818d7b32d31964921210ab55
Sikum Limbu <134518156+SikumLibang@users.noreply.github.com>
2023-12-04 22:30:23 +0545

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -1,21 +1,10 @@
 <?php
-if (isset($_POST['btn-send'])) {
-    $userName = $_POST['uname'];
-    $userEmail = $_POST['email'];
-    $userNumber = $_POST['number'];
-    $emailSubject = $_POST['subject'];
-    $userMessage = $_POST['message'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $to = "imsikumlimbu@gmail.com";
+    $subject = $_POST["subject"] ?? "No Subject";
+    $message = $_POST["message"] ?? "No Message";
+    $headers = "From: " . $_POST["email"];
 
-    $to = "skllimbu2005@gmail.com";
-    $subject = "Contact Form: $emailSubject";
-    $body = "Name: $userName\nEmail: $userEmail\nMobile Number: $userNumber\n\nMessage:\n$userMessage";
-
-    $headers = "From: $userEmail";
-
-    if (mail($to, $subject, $body, $headers)) {
-        echo "<p>Thank you for contacting us. Your message has been sent successfully.</p>";
-    } else {
-        echo "<p>Sorry, there was an error sending your message. Please try again later.</p>";
-    }
+    mail($to, $subject, $message, $headers);
 }
-?>    
+?>
